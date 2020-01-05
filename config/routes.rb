accb985364c2b6ae5770c7d88834a9b6882aec7f
Neah Nieto <nrpcnieto@gmail.com>
2020-01-05 10:35:34 -0600

    Add MVC for tea API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/pages" => "pages#index"
+
+    get "/tea" => "tea#index"
+    post "/tea" => "tea#create"
+    get "/tea/:id" => "tea#show"
+    patch "/tea/:id" => "tea#update"
+    delete "/tea/:id" => "tea#destroy"
   end
 end
